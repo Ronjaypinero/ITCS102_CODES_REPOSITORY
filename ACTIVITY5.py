d55badcd43ee46abe26da83027345956df73c4b6
Ronjaypinero <ronjaypinero2@gmail.com>
2026-08-25 13:00:45 +0800

    Add files via upload

---
+++ ACTIVITY5.py
@@ -0,0 +1,18 @@
+#ggggggggggg
+a=71
+b=24
+sub = a - b
+sum= a + b
+mul= a * b
+div= a / b
+flv= a // b
+exp= a ** b
+mod= a % b
+
+print("the sub of a and b is", sub)
+print("the sum of a and b is", sum)
+print("the product of a and b is", mul)
+print("the division of a and b is", div)
+print("the round down of a and b is", flv)
+print("the exponents of a and b is", exp)
+print("the modulus of a and b is", mod)
