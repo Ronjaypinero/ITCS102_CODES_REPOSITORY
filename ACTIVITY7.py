82b404b6b209afb116ed7f51272477a58c1ce846
Ronjaypinero <ronjaypinero2@gmail.com>
2026-09-03 12:39:51 +0800

    Add files via upload

---
+++ ACTIVITY7.py
@@ -0,0 +1,28 @@
+total=0
+
+n1=eval(input("enter a number--> "))
+total= total + n1
+#total1=total+n1
+
+
+n2=eval(input("enter a number--> "))
+total = total + n2
+
+n3=eval(input("enter a number--> "))
+total *= total + n3
+
+n4=eval(input("enter a number--> "))
+total += total + n4
+
+n5=eval(input("enter a number--> "))
+total -= total + n5
+
+n6=eval(input("enter a number--> "))
+total *= total + n6
+
+n7=eval(input("enter a number--> "))
+total += total + n7
+
+
+
+print("The sum is", total)
