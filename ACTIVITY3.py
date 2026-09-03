15940d4b13783dd6e532798168878b2fd170290f
Ronjaypinero <ronjaypinero2@gmail.com>
2026-09-03 12:31:42 +0800

    Add files via upload

---
+++ ACTIVITY3.py
@@ -0,0 +1,15 @@
+#integer variable 
+MyAge = 18
+MyBirthdate = 2_4_2008
+
+#Float Variable
+MyHeight = 5_7
+MyFeetSize = 43
+
+#String Variable
+MyAdress = Sariaya
+
+#Boolean Variable
+MyFiendIsGay = True
+
+print("Hi, my age is ", MyAge)
