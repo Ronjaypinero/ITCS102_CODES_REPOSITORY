02e56375c9ea613c04f95fffe93da6a19c7be2b9
Ronjaypinero <ronjaypinero2@gmail.com>
2026-09-08 12:46:20 +0800

    Add files via upload

---
+++ ACTIVITY10.py
@@ -0,0 +1,17 @@
+R = 2
+C = 7
+B = 15
+
+#1
+print( C > B and R < C)
+
+#2
+print(not (R <= B or C != R and C > R))
+
+R = 'GWO'
+U = 'RUN'
+N = 'RUN'
+
+#3
+print( R != U or N <= R)
+print( N == U and R == N or U > R and R != N)
