9ad398bd6b73db6e9ea9d96c3d8dded048cd2fca
Ronjaypinero <ronjaypinero2@gmail.com>
2026-09-10 12:55:23 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,13 @@
+NAME = (input("SENDER NAME: "))
+
+ITEM = (input("WHAT TYPE OF ITEM IS IT: "))
+
+ISFRAGILE = bool(input("IS IT FRAGILE? "))
+
+WEIGHT = float(input("WEIGHT: "))
+
+DISTANCE = float(input("DISTANCE: "))
+
+is_express = bool(input("IS_EXPRESS: "))
+
+IS_INTERNATIONAL = 
